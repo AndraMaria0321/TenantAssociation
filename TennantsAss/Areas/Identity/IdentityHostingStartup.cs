17c4fd599c423497b34214217ea930359aae92d6
edith99 <edith.dragoi@gmail.com>
2021-04-14 15:39:52 +0300

    CommitByED

---
+++ TennantsAss/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TennantsAss.Data;
+
+[assembly: HostingStartup(typeof(TenantsAss.Areas.Identity.IdentityHostingStartup))]
+namespace TenantsAss.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ TennantsAss/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TenantsAss.Models;
+
+namespace TennantsAss.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+           public DbSet<UserType> UsersType { get; set; }
+           public DbSet<User> Users { get; set; }
+
+
+    }
+}
