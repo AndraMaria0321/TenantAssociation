2bc2284790e470d1b1530ebda80a310f1efa4d38
edith99 <edith.dragoi@gmail.com>
2021-05-24 20:51:07 +0300

    Automated tests

---
+++ TenantsAss.AutomatedTests/PageObjects/ApartmentIndexPage.cs
@@ -0,0 +1,42 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TenantsAss.AutomatedTests.PageObjects
+{
+    class ApartmentIndexPage
+    {
+        private IWebDriver webDriver;
+
+        [FindsBy(How = How.CssSelector, Using = "table")]
+        private IWebElement apartmentList;
+
+        [FindsBy(How = How.LinkText, Using = "Create New")]
+        private IWebElement addApartmentButton;
+
+        public ApartmentIndexPage(IWebDriver driver)
+        {
+            webDriver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+        public void GoToPage()
+        {
+            webDriver.Navigate().GoToUrl("https://localhost:44346/Apartments");
+        }
+        public AddApartmentPage GotoAddApartmentPage()
+        {
+            addApartmentButton.Click();
+            return new AddApartmentPage(webDriver);
+        }
+
+        public bool ApartmentExists(string streetName)
+        {
+            var elements = apartmentList.FindElements(By.TagName("td"));
+            return elements.Where(element => element.Text.Equals(streetName)).Count() > 0;
+
+        }
+    }
+}
---
+++ TenantsAss.AutomatedTests/PageObjects/BuildingIndexPage.cs
@@ -0,0 +1,42 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TenantsAss.AutomatedTests.PageObjects
+{
+    class BuildingIndexPage
+    {
+        private IWebDriver webDriver;
+
+        [FindsBy(How = How.CssSelector, Using = "table")]
+        private IWebElement buildingList;
+
+        [FindsBy(How = How.LinkText, Using = "Add new building details")]
+        private IWebElement addBuildingButton;
+
+        public BuildingIndexPage(IWebDriver driver)
+        {
+            webDriver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+        public void GoToPage()
+        {
+            webDriver.Navigate().GoToUrl("https://localhost:44346/Building");
+        }
+        public AddBuildingPage GotoAddBuildingPage()
+        {
+            addBuildingButton.Click();
+            return new AddBuildingPage(webDriver);
+        }
+
+        public bool BuildingExists(string streetName)
+        {
+            var elements = buildingList.FindElements(By.TagName("td"));
+            return elements.Where(element => element.Text.Equals(streetName)).Count() > 0;
+
+        }
+    }
+}
